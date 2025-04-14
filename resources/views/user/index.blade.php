1ac5cd61ab2c703abd27a0c484dcca876ee05a85
RizkyRR27 <rizkyroza2005@gmail.com>
2025-04-14 18:32:07 +0700

    implemen js6

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -6,50 +6,61 @@
         <h3 class="card-title">{{ $page->title }}</h3>
         <div class="card-tools">
             <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a>
+            <button onclick="modalAction('{{ url('/user/create_ajax') }}')" class="btn btn-sm btn-success mt-1">tambah ajax</buttono>
         </div>
     </div>
     <div class="card-body">
-    @if (session('success'))
+        @if (session('success'))
         <div class="alert alert-success">{{ session('success') }}</div>
-    @endif
-    @if (session('error'))
-    <div class="alert alert-success">{{ session('error') }}</div>
-    @endif
-    <div class="row">
-    <div class="col-md-12">
-        <div class="form-group row">
-            <label class="col-1 control-label col-form-label">Filter:</label>
-            <div class="col-3">
-                <select class="form-control" id="level_id" name="level_id" required>
-                    <option value="">- Semua -</option>
-                    @foreach($level as $item)
-                        <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
-                    @endforeach
-                </select>
-                <small class="form-text text-muted">Level Pengguna</small>
+        @endif
+        @if (session('error'))
+        <div class="alert alert-success">{{ session('error') }}</div>
+        @endif
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label">Filter:</label>
+                    <div class="col-3">
+                        <select class="form-control" id="level_id" name="level_id" required>
+                            <option value="">- Semua -</option>
+                            @foreach($level as $item)
+                            <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                            @endforeach
+                        </select>
+                        <small class="form-text text-muted">Level Pengguna</small>
+                    </div>
+                </div>
             </div>
         </div>
+        <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Nama</th>
+                    <th>Level Pengguna</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+        </table>
     </div>
-</div>
-    <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Nama</th>
-                <th>Level Pengguna</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-    </table>
-</div>
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog"
+        data- backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true">
+    </div>
 
 </div> @endsection
 @push('css') @endpush
 @push('js')
 <script>
+    function modalAction(url = '') {
+        $('#myModal').load(url, function() {
+            $('#myModal').modal('show');
+        });
+    }
+
+    var dataUser;
     $(document).ready(function() {
-        var dataUser = $('#table_user').DataTable({
+        dataUser = $('#table_user').DataTable({
             processing: true,
             serverSide: true,
             ajax: {
@@ -69,7 +80,7 @@
                 data: "username",
                 className: "",
                 orderable: true,
-                searchable: true, 
+                searchable: true,
             }, {
                 data: "nama",
                 className: "",
@@ -91,5 +102,5 @@
             dataUser.ajax.reload();
         });
     });
-</script> 
+</script>
 @endpush
