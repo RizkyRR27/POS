77e38273100bf0e389b6799aba46eb7f9df98a87
RizkyRR27 <rizkyroza2005@gmail.com>
2025-04-14 18:41:33 +0700

    js 7

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,19 +6,50 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Symfony\Component\VarDumper\Exception\ThrowingCasterException;
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
-    
     use HasFactory;
 
-    protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id'; //Mendefinisikan primary key dari tabel yang digunakan
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
+
+    protected $hidden = ['password']; // jangan di tampilkan saat select
+
+    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
 
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
-    public function level (): BelongsTo
+    public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
+
+    public function getRoleName() {
+        return $this->level->level_nama;
+    }
+    public function hasRole($role) {
+        return $this->level->level_kode == $role;
+    }
+    public function getRole(){
+        return $this->level->level_kode;
+    }
 }
+
+// class UserModel extends Model
+// {
+    
+//     use HasFactory;
+
+//     protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model ini
+//     protected $primaryKey = 'user_id'; //Mendefinisikan primary key dari tabel yang digunakan
+
+
+//     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+//     public function level (): BelongsTo
+//     {
+//         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+//     }
+// }
