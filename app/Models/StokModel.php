626686dc1504d7df2febde05f6f731a8721ebbd4
RizkyRR27 <rizkyroza2005@gmail.com>
2025-04-14 18:57:24 +0700

    buat stok model

---
+++ app/Models/StokModel.php
@@ -0,0 +1,30 @@
+<?php
+ 
+ namespace App\Models;
+ 
+ use Illuminate\Database\Eloquent\Factories\HasFactory;
+ use Illuminate\Database\Eloquent\Model;
+ use Illuminate\Database\Eloquent\Relations\BelongsTo;
+ 
+ class StokModel extends Model
+ {
+     use HasFactory;
+ 
+     protected $table = "t_stok";
+     protected $primaryKey = "stok_id";
+     protected $fillable = [ 'barang_id', 'user_id', 'stok_jumlah'];
+ 
+     public $timestamps = true; // Aktifkan timestamps
+ 
+     // Hubungann ke barang
+     public function barang(): BelongsTo
+     {
+         return $this->belongsTo(BarangModel::class, 'barang_id', 'barang_id');
+     }
+ 
+     // Relasi ke user
+     public function user(): BelongsTo
+     {
+         return $this->belongsTo(UserModel::class, 'user_id', 'user_id'); 
+     }
+ }
